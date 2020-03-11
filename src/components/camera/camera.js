9b26ccec46135025f615ebe2c284849bd1fa912d
jsgalvish <jsgalvish@unal.edu.co>
2020-03-11 17:16:52 -0500

    get location photo

--- src/components/camera/camera.js
+++ src/components/camera/camera.js
@@ -3,24 +3,28 @@ import Camera from 'react-html5-camera-photo';
 import download from 'downloadjs';
 
 class Camara extends React.Component {
-    
-  
-handleTakePhoto (dataUri) {
-    // Do stuff with the photo...
-    console.log('takePhoto');
-    console.log(dataUri)
-    
-    download(dataUri, "dlDataUrlText.png", "image/png");
-  }
+
+    handleTakePhoto (dataUri) {
+        console.log('Location');
+        navigator.geolocation.getCurrentPosition(position => {
+            const latitude  = position.coords.latitude;
+            const longitude = position.coords.longitude;
+            console.log(latitude, longitude)
+        });
+        console.log('takePhoto');
+        console.log(dataUri)
+        download(dataUri, "dlDataUrlText.png", "image/png");
+    }
 
     render() {
         return(
             <div>
                 <h1>camera</h1>
                 <Camera
-      onTakePhoto = { (dataUri) => {this.handleTakePhoto(dataUri);
-      } }
-    />
+                    onTakePhoto = { (dataUri) => {
+                        this.handleTakePhoto(dataUri);
+                    } }
+                />
             </div>
 
         );
